e74734f16be72a0735266865d1d5f4a62c4be55c
volpe23 <arozmianecs@gmail.com>
2023-01-21 21:13:24 +0200

    Added modal and player choices

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ function createSquare(index) {
 
     return div;
 }
+
+const loadChoices = (() => {
+    const choice1 = document.querySelector('#player1');
+    const choice2 = document.querySelector('#player2');
+    choice2.value = 'o'
+    return { choice1, choice2 }
+})()
 // Create game board 
 const gameBoard = (() => {
     const board = document.querySelector('.game-board');
@@ -17,17 +24,17 @@ const gameBoard = (() => {
         
     };
     const addChoice = (e, i) => {
-        const symbol = game.getTurn();
-        if (isWinner(symbol) === false) {
-            e.target.classList.add(`${symbol}`);
-            gameFields[i] = game.getTurn();
+        const player = game.getTurn();
+        if (isWinner(player.symbol) === false) {
+            e.target.classList.add(`${player.symbol}`);
+            gameFields[i] = player.symbol;
             
-            if (isWinner(symbol)) game.gameOver(symbol) 
+            if (isWinner(player.symbol)) game.gameOver(player) 
             else game.switchTurn()
         } 
-        else if (isWinner(symbol) === null) game.gameOver(symbol, null);
+        else if (isWinner(player.symbol) === null) game.gameOver(null);
         else {
-            game.gameOver(symbol);
+            game.gameOver(player);
             
         };
     };
@@ -50,37 +57,56 @@ const gameBoard = (() => {
     return { fillGameBoard, addChoice };
 })();
 
-const player = (inp) => {
+const player = (inp, name='Player1') => {
+    let playerName = name;
     let score = 0;
     const symbol = inp;
     const addScore = () => score++;
-    return { symbol, addScore };
+    return { playerName, symbol, addScore };
 };
+
+const setPlayers = () => {
+    console.log('Hey');
+}
 
 const game = (() => {
     let player1;
     let player2;
     let xTurn;
     const startGame = () => {
+        if (loadChoices.choice1.value === loadChoices.choice2.value) return console.log('Invalid symbols')
         gameBoard.fillGameBoard();
-        const choice1 = document.querySelector('#player1');
-        const choice2 = document.querySelector('#player2');
-        player1 = player(choice1.value);
-        player2 = player(choice2.value);
+        loadChoices.choice1.disabled = true;
+        loadChoices.choice2.disabled = true;
+        player1 = player(loadChoices.choice1.value, 'Player1');
+        player2 = player(loadChoices.choice2.value, 'Player2');
     };
     const getTurn = () => {
         const currentTurn = xTurn ? player2 : player1;
-        return currentTurn.symbol;
+        return currentTurn;
     };
     const switchTurn = () => {
         xTurn = !xTurn;
     };
-    const gameOver = (symbol, draw) => {
-        if (draw === null) console.log('DRAW!');
-        else console.log(`${symbol} wins!`);
+    const gameOver = (player) => {
+        const modal = document.querySelector('.modal');
+        const overlay = document.querySelector('#overlay');
+        const winnerMessage = document.querySelector('#winner-message');
+        if (player.symbol === null) console.log('DRAW!');
+        else {
+            winnerMessage.textContent = `${player.playerName} wins`;
+            modal.classList.add('active');
+            overlay.classList.add('active');
+        };
     }
     document.querySelector('#startGame').addEventListener('click', () => startGame());
     return { player1, player2, getTurn, switchTurn, gameOver };
 })();
+
+document.querySelector('#close').addEventListener('click', (e) => {
+    console.log('Close modal');
+    e.target.closest('#overlay').classList.remove('active');
+    e.target.closest('.modal').classList.remove('active');
+})
 
 gameBoard.fillGameBoard()
